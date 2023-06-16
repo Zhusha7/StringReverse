5774d259f04fe1786a11431e1c1c304a4b6c79b4
Andzej Siniakov <andzej200@gmail.com>
2023-06-16 19:50:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringReverse
+{
+    internal class Program
+    {
+        static string? ReverseString(string? s)
+        {
+            if (s == null)
+                return null;
+            char[] temp = s.ToCharArray();
+            for (int i = 0; i < temp.Length/2; i++)
+            {
+                (temp[i], temp[^(i+1)]) = (temp[^(i+1)], temp[i]);
+            }
+            return new string(temp);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString(Console.ReadLine()));
+        }
+    }
+}
